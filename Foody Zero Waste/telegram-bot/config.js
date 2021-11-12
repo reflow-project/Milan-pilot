0a8819ba8539997b670ffd72935a44f3ac693336
vcuculo <vittorio@dotdotdot.it>
2021-11-12 12:39:06 +0100

    remove and revoke old bot token

--- Foody Zero Waste/telegram-bot/config.js
+++ Foody Zero Waste/telegram-bot/config.js
@@ -3,7 +3,6 @@ var config = {};
 config.bot = {};
 config.reflow = {};
 
-config.bot.token = '1872583825:AAFBEZhYVPvR0gtE7_HKLa6unXM3sysPQfg';
 config.bot.polling_time = 10000; // 10 sec - how often check for new donations
 config.bot.donation_timeout = 1800000; // 30 min - how much time before automatically refuse a donation
 
